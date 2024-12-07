260de6d7fa08ac5f1d956eb05a6623e01fa88fb9
usmansbk <usmansbk@gmail.com>
2024-12-07 17:09:08 +0100

    animate logo ticker

--- src/app/sections/LogoTicker.tsx
+++ src/app/sections/LogoTicker.tsx
@@ -1,3 +1,4 @@
+"use client";
 import acmeLogo from "@/assets/logos/logo-acme.png";
 import apexLogo from "@/assets/logos/logo-apex.png";
 import celestialLogo from "@/assets/logos/logo-celestial.png";
@@ -5,6 +6,7 @@ import echoLogo from "@/assets/logos/logo-echo.png";
 import pulseLogo from "@/assets/logos/logo-pulse.png";
 import quantumLogo from "@/assets/logos/logo-quantum.png";
 import Image from "next/image";
+import { motion } from "motion/react";
 
 const logos = [
   {
@@ -38,7 +40,18 @@ export default function LogoTicker() {
     <div className="py-8 mb:py-12 bg-white">
       <div className="container">
         <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
-          <div className="flex gap-14 flex-none">
+          <motion.div
+            className="flex gap-14 flex-none pr-14"
+            animate={{
+              translateX: "-50%",
+            }}
+            transition={{
+              duration: 20,
+              ease: "linear",
+              repeat: Infinity,
+              repeatType: "loop",
+            }}
+          >
             {logos.map((logo) => (
               <Image
                 key={logo.name}
@@ -47,7 +60,15 @@ export default function LogoTicker() {
                 className="logo-ticker-image"
               />
             ))}
-          </div>
+            {logos.map((logo) => (
+              <Image
+                key={logo.name}
+                src={logo.image}
+                alt={logo.name}
+                className="logo-ticker-image"
+              />
+            ))}
+          </motion.div>
         </div>
       </div>
     </div>
