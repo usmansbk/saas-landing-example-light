2e267eb96a4b4dbed125819070b693bd4fe9da93
usmansbk <usmansbk@gmail.com>
2024-12-07 09:50:11 +0100

    add pricing

---
+++ src/app/sections/Pricing.tsx
@@ -0,0 +1,138 @@
+import CheckIcon from "@/assets/icons/check.svg";
+import clsx from "clsx";
+
+const pricingTiers = [
+  {
+    title: "Free",
+    monthlyPrice: 0,
+    buttonText: "Get started for free",
+    popular: false,
+    inverse: false,
+    features: [
+      "Up to 5 project members",
+      "Unlimited tasks and projects",
+      "2GB storage",
+      "Integrations",
+      "Basic support",
+    ],
+  },
+  {
+    title: "Pro",
+    monthlyPrice: 9,
+    buttonText: "Sign up now",
+    popular: true,
+    inverse: true,
+    features: [
+      "Up to 50 project members",
+      "Unlimited tasks and projects",
+      "50GB storage",
+      "Integrations",
+      "Priority support",
+      "Advanced support",
+      "Export support",
+    ],
+  },
+  {
+    title: "Business",
+    monthlyPrice: 19,
+    buttonText: "Sign up now",
+    popular: false,
+    inverse: false,
+    features: [
+      "Up to 5 project members",
+      "Unlimited tasks and projects",
+      "200GB storage",
+      "Integrations",
+      "Dedicated account manager",
+      "Custom fields",
+      "Advanced analytics",
+      "Export capabilities",
+      "API access",
+      "Advanced security features",
+    ],
+  },
+];
+
+export default function Pricing() {
+  return (
+    <section className="py-24 bg-white">
+      <div className="container">
+        <div className="section-heading">
+          <h2 className="section-title">Pricing</h2>
+          <p className="section-description mt-5">
+            Free forever. Upgrade for unlimited tasks, better security, and
+            exclusive features.
+          </p>
+        </div>
+        <div className="flex flex-col lg:flex-row gap-6 items-center mt-10 lg:items-end lg:justify-center">
+          {pricingTiers.map(
+            ({
+              title,
+              monthlyPrice,
+              buttonText,
+              popular,
+              inverse,
+              features,
+            }) => (
+              <div
+                key={title}
+                className={clsx(
+                  "p-10 rounded-3xl shadow-[0_7px_14px_#eaeaea] max-w-xs w-full",
+                  inverse && "bg-black text-white border-black"
+                )}
+              >
+                <div className="flex justify-between">
+                  <h3
+                    className={clsx(
+                      "text-lg font-bold text-black/50",
+                      inverse && "text-white/60"
+                    )}
+                  >
+                    {title}
+                  </h3>
+                  <div
+                    className={clsx(
+                      "text-sm px-4 py-1.5 rounded-xl border border-white/20",
+                      popular ? "inline-flex" : "hidden"
+                    )}
+                  >
+                    <span className="bg-[linear-gradient(to_right,#dd7ddf,#e1cd86,#bbcb92,#71c2ef,#3bffff,#dd7ddf)] text-transparent bg-clip-text font-medium">
+                      Popular
+                    </span>
+                  </div>
+                </div>
+                <div className="flex items-baseline gap-1 mt-[30px]">
+                  <span className="text-4xl font-bold tracking-tighter leading-none">
+                    ${monthlyPrice}
+                  </span>
+                  <span className="tracking-tight font-bold text-black/50">
+                    /month
+                  </span>
+                </div>
+                <button
+                  className={clsx(
+                    "btn btn-primary w-full mt-[30px]",
+                    inverse && "bg-white text-black"
+                  )}
+                >
+                  {buttonText}
+                </button>
+                <ul className="flex flex-col gap-5 mt-8">
+                  {features.map((feature) => (
+                    <li
+                      key={feature}
+                      className="text-sm flex items-center gap-4"
+                    >
+                      <CheckIcon className="h-6 w-6" />
+                      <span>{feature}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )
+          )}
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/app/sections/ProductShowcase.tsx
+++ src/app/sections/ProductShowcase.tsx
@@ -7,14 +7,14 @@ export default function ProductShowcase() {
   return (
     <section className="bg-gradient-to-b from-white to-[#d2dcff] py-24 overflow-x-clip">
       <div className="container">
-        <div className="max-w-[540px] mx-auto">
+        <div className="section-heading">
           <div className="flex justify-center">
             <div className="tag">Boost productivity</div>
           </div>
-          <h2 className="text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001e80] text-transparent bg-clip-text mt-5">
+          <h2 className="section-title mt-5">
             A more effective way to track progress
           </h2>
-          <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010d3e] mt-5">
+          <p className="section-description mt-5">
             Effortlessly turn your ideas into a fully functional, responsive,
             no-code SaaS website in just minutes with the set of free components
             for Framer.
