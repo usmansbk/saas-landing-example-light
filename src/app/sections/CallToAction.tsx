409f15e664a87a569da69e8a8413d5ee18d2e04f
usmansbk <usmansbk@gmail.com>
2024-12-07 22:39:15 +0100

    animate cta

--- src/app/sections/CallToAction.tsx
+++ src/app/sections/CallToAction.tsx
@@ -1,11 +1,25 @@
+"use client";
 import ArrowRight from "@/assets/icons/arrow-right.svg";
 import springImage from "@/assets/images/spring.png";
 import starImage from "@/assets/images/star.png";
-import Image from "next/image";
+import { motion, useScroll, useTransform } from "motion/react";
+import { useRef } from "react";
 
 export default function CallToAction() {
+  const sectionRef = useRef<HTMLElement>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
+
   return (
-    <section className="bg-gradient-to-b from-white to-[#d2dcff] py-24 overflow-x-clip">
+    <section
+      ref={sectionRef}
+      className="bg-gradient-to-b from-white to-[#d2dcff] py-24 overflow-x-clip"
+    >
       <div className="container">
         <div className="section-heading relative">
           <h2 className="section-title">Sign up for free today</h2>
@@ -13,17 +27,23 @@ export default function CallToAction() {
             Celebrate the joy of accomplishment with an app designed to track
             your progress and motivate your efforts.
           </p>
-          <Image
-            src={starImage}
+          <motion.img
+            src={starImage.src}
             alt="Star Image"
             width={360}
             className="hidden md:block absolute -left-[350px] -top-[137px]"
+            style={{
+              translateY,
+            }}
           />
-          <Image
-            src={springImage}
+          <motion.img
+            src={springImage.src}
             alt="Spring Image"
             width={360}
             className="hidden md:block absolute -right-[331px] -top-[19px]"
+            style={{
+              translateY,
+            }}
           />
         </div>
         <div className="flex gap-2 mt-10 justify-center">
